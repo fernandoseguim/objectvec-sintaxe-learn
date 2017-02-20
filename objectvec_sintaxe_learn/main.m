73499ab9cf3660898be7fed68618274d543ffaa8
Fernando Seguim <fernando.seguim@gmail.com>
2017-02-20 13:53:51 -0300

    Initial Commit

---
+++ objectvec_sintaxe_learn/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  objectvec_sintaxe_learn
+//
+//  Created by Fernando Seguim on 20/02/17.
+//  Copyright © 2017 Fernando Seguim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
